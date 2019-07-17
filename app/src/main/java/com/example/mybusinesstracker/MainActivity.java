221f1973aa5086f708fdfa250bfaeb41dc2ce0ed
pa1-teja <pavanteja.93@gmail.com>
2019-07-17 17:12:53 +0530

    added navigation drawer.

--- app/src/main/java/com/example/mybusinesstracker/MainActivity.java
+++ app/src/main/java/com/example/mybusinesstracker/MainActivity.java
@@ -1,140 +1,82 @@
 package com.example.mybusinesstracker;
 
 import android.os.Bundle;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
-import androidx.appcompat.widget.Toolbar;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import com.example.mybusinesstracker.BaseCalsses.BaseActivity;
-import com.example.mybusinesstracker.cloud_firestore.TestDB;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.firestore.DocumentReference;
-import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.android.material.navigation.NavigationView;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.Objects;
 
-public class MainActivity extends BaseActivity implements OnCompleteListener<Void>, OnFailureListener, OnSuccessListener {
+public class MainActivity extends AppCompatActivity {
 
+    ActionBarDrawerToggle t;
+    DrawerLayout dl;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        final TestDB testDocument = new TestDB();
+        dl = findViewById(R.id.activity_main);
+        Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
+        t = new ActionBarDrawerToggle(this, dl, R.string.Open, R.string.Close);
+        dl.addDrawerListener(t);
+        t.syncState();
 
-        findViewById(R.id.add).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                //addData();
-                /*CollectionReference collectionReference = testDocument.getCollection("mybusinesstracker");
-                DocumentReference documentReference = testDocument.getDocument(collectionReference,"test1");
-                HashMap<String, String> test = new HashMap<>();
-                test.put("date","08/07/2019");
-                test.put("blocks","10");
-                test.put("amount","2000");
-                testDocument.addDataField(documentReference,test,this);*/
-                Toast.makeText(MainActivity.this,"new data added", Toast.LENGTH_LONG).show();
-            }
-        });
-        findViewById(R.id.update).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+        //getActionBar().setDisplayHomeAsUpEnabled(true);
+        //getSupportActionBar().setDisplayHomeAsUpEnabled(t
+        //
+        // rue);
 
-            }
-        });
-        findViewById(R.id.delete).setOnClickListener(new View.OnClickListener() {
+
+
+
+
+
+
+
+        
+        NavigationView nv = findViewById(R.id.nv);
+        nv.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
             @Override
-            public void onClick(View v) {
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int id = item.getItemId();
+                switch(id)
+                {
+                    case R.id.account:
+                        Toast.makeText(MainActivity.this, "My Account",Toast.LENGTH_SHORT).show();break;
+                    case R.id.settings:
+                        Toast.makeText(MainActivity.this, "Settings",Toast.LENGTH_SHORT).show();break;
+                    case R.id.mycart:
+                        Toast.makeText(MainActivity.this, "My Cart",Toast.LENGTH_SHORT).show();break;
+                    default:
+                        return true;
+                }
+
+
+                return true;
 
             }
         });
     }
-    OnFailureListener onFailureListener = new OnFailureListener() {
-        @Override
-        public void onFailure(@NonNull Exception e) {
-
-        }
-    };
-    OnSuccessListener<? super Void> onSuccessListener = new OnSuccessListener<Void>() {
-        @Override
-        public void onSuccess(Void aVoid) {
-
-        }
-    };
-    OnCompleteListener<Void> onCompleteListener = new OnCompleteListener<Void>() {
-        @Override
-        public void onComplete(@NonNull Task<Void> task) {
-
-        }
-    };
-    private void addData() {
-        FirebaseFirestore db = FirebaseFirestore.getInstance();
-        Map<String, Object> user = new HashMap<>();
-        user.put("first", "Ada");
-        user.put("last", "Lovelace");
-        user.put("born", 1815);
-
-// Add a new document with a generated ID
-        db.collection("users")
-                .add(user)
-                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
-                    @Override
-                    public void onSuccess(DocumentReference documentReference) {
-                        Log.d(TAG, "DocumentSnapshot added with ID: " + documentReference.getId());
-                    }
-                })
-                .addOnFailureListener(new OnFailureListener() {
-                    @Override
-                    public void onFailure(@NonNull Exception e) {
-                        Log.w(TAG, "Error adding document", e);
-                    }
-                });
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if(t.onOptionsItemSelected(item)) {
             return true;
         }
 
         return super.onOptionsItemSelected(item);
     }
-
     @Override
-    public void onComplete(@NonNull Task<Void> task) {
-        Toast.makeText(MainActivity.this, "onComplete", Toast.LENGTH_SHORT).show();
-    }
-
-    @Override
-    public void onFailure(@NonNull Exception e) {
-        Toast.makeText(MainActivity.this, "onFailure", Toast.LENGTH_SHORT).show();
-    }
-
-    @Override
-    public void onSuccess(Object o) {
-        Toast.makeText(MainActivity.this, "onSuccess", Toast.LENGTH_SHORT).show();
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
     }
 }
